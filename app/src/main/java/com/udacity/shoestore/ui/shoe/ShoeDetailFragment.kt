d30040c84377810788d1b9e7f4a8da889dde31ec
Piotr <piotr.liszka.pl@gmail.com>
2020-12-15 05:58:08 +0100

    11. Create a new Shoe List

---
+++ app/src/main/java/com/udacity/shoestore/ui/shoe/ShoeDetailFragment.kt
@@ -0,0 +1,6 @@
+package com.udacity.shoestore.ui.shoe
+
+import androidx.fragment.app.Fragment
+import com.udacity.shoestore.R
+
+class ShoeDetailFragment: Fragment(R.layout.fragment_shoe_detail)
--- app/src/main/java/com/udacity/shoestore/ui/shoe/ShoeListFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ui/shoe/ShoeListFragment.kt
@@ -1,6 +1,38 @@
 package com.udacity.shoestore.ui.shoe
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentShoeListBinding
 
-class ShoeListFragment: Fragment(R.layout.fragment_shoe_list)
+class ShoeListFragment : Fragment() {
+
+    private lateinit var binding: FragmentShoeListBinding
+    private lateinit var viewModel: ShoeListViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_shoe_list, container, false)
+        viewModel = ViewModelProvider(this).get(ShoeListViewModel::class.java)
+
+        viewModel.shoeList.observe(viewLifecycleOwner, Observer {
+            // ToDo show shoe item
+        })
+
+        binding.shoeListAddBtn.setOnClickListener {
+            findNavController().navigate(ShoeListFragmentDirections.actionShoeListFragmentToShoeDetailFragment())
+        }
+
+        return binding.root
+    }
+}
