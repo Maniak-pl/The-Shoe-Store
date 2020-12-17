3383eb898dd8f14e66610445fdabb00cf195cab2
Piotr <piotr.liszka.pl@gmail.com>
2020-12-17 05:46:05 +0100

    Binding in ShoeDetailFragment.kt

--- app/src/main/java/com/udacity/shoestore/ui/shoe/ShoeDetailFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ui/shoe/ShoeDetailFragment.kt
@@ -10,11 +10,13 @@ import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentShoeDetailBinding
+import com.udacity.shoestore.models.Shoe
 import kotlinx.android.synthetic.main.fragment_shoe_detail.*
 
 class ShoeDetailFragment : Fragment() {
     private lateinit var binding: FragmentShoeDetailBinding
     private val viewModel: ShoeViewModel by activityViewModels()
+    private lateinit var shoe: Shoe
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,15 +24,13 @@ class ShoeDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_shoe_detail, container, false)
+        shoe = Shoe("",0.0, "","")
+        binding.shoe = shoe
 
         binding.saveBtn.setOnClickListener {
-            val size = sizeInput.text.toString()
-            viewModel.addShoe(
-                nameInput.text.toString(),
-                if(size.isNotEmpty()) size.toDouble() else 0.0,
-                companyInput.text.toString(),
-                descriptionInput.text.toString()
-            )
+//            val size = binding.size.text.toString()
+//            shoe.size = if(size.isNotEmpty()) size.toDouble() else 0.0
+            viewModel.addShoe(shoe)
 
             navigateBack()
         }
--- app/src/main/java/com/udacity/shoestore/ui/shoe/ShoeViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/ui/shoe/ShoeViewModel.kt
@@ -1,9 +1,13 @@
 package com.udacity.shoestore.ui.shoe
 
+import android.widget.EditText
+import androidx.databinding.BindingAdapter
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.udacity.shoestore.models.Shoe
+import java.text.DecimalFormat
+
 
 class ShoeViewModel : ViewModel() {
 
@@ -18,5 +22,24 @@ class ShoeViewModel : ViewModel() {
     fun addShoe(name: String, size: Double, company: String, description: String) {
         _shoeList.value?.add(Shoe(name, size, company, description))
         _shoeList.value = _shoeList.value
+    }
+
+    fun addShoe(shoe: Shoe) {
+        _shoeList.value?.add(shoe)
+        _shoeList.value = _shoeList.value
+    }
+}
+
+@BindingAdapter("size")
+fun setVal(editText: EditText, newVal: Double) {
+    val currentValue = editText.text.toString()
+    try {
+        if (java.lang.Double.valueOf(currentValue) != newVal) {
+            val decimalFormat = DecimalFormat("#.##")
+            val `val`: String = decimalFormat.format(newVal)
+            editText.setText(`val`)
+        }
+    } catch (exception: NumberFormatException) {
+        exception.printStackTrace()
     }
 }
